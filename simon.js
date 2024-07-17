e38c897139fc03cff05b1a901910980209ec28f5
piyanshusahu <164355422+piyanshusahu@users.noreply.github.com>
2024-07-17 10:19:09 +0530

    Update simon.js

--- simon.js
+++ simon.js
@@ -1,39 +1,39 @@
-let userseq=[];
-let gameseq=[];
+let userseq=[];  // keeping track of what user has selected (colour sequence). It gets reset after every level up.
+let gameseq=[];  // keepint track of what user selected from starting. It only gets reset after match is over
 let colour=["red","yellow","green","blue"];
 let start=false;
-document.addEventListener("keypress",function(){
+let h2=document.querySelector("#key");
+let level=0;
+document.addEventListener("keypress",function(){  // any key pressed from keyboard starts the game
     if(start===false){
-        levelup();
+        levelup(); // starting the game.
     }
     start=true;
 });
-let h2=document.querySelector("#key");
 
-let level=0;
 function levelup(){
-    userseq=[];
+    userseq=[]; // after every level up userseq gets reset to empty. 
     level++;
     h2.innerText=`level ${level}`;
-    let randomNumber=Math.floor(Math.random()*4);
-    let randomColour=colour[randomNumber];
-    let btn=document.querySelector(`.${randomColour}`);
-    btn.classList.add("gameflash");
-    gameseq.push(randomColour);
+    let randomNumber=Math.floor(Math.random()*4); // generating random value upto 4.
+    let randomColour=colour[randomNumber];  // choosing the colour based on their index.
+    let btn=document.querySelector(`.${randomColour}`);  
+    btn.classList.add("gameflash");  // adding a gameflash class which flases the button clicked.
+    gameseq.push(randomColour);  
     setTimeout(()=> {
         btn.classList.remove("gameflash")
-    },250);
+    },250);  // removing the gameflash.
 }
 
 function check(idx){
-    if(userseq[idx]===gameseq[idx]){
-        if(userseq.length==gameseq.length){
+    if(userseq[idx]===gameseq[idx]){            // if idx index colour is same in userseq and gameseq then it checks whether last entered index was the las index or not.
+        if(userseq.length==gameseq.length){  // if it was the last index then level up.
             setTimeout(() => {
                 levelup();
             },500);
         }
     }
-    else{
+    else{                                                  // if idx index colour does not matches then reset everything.
         document.querySelector("body").style.backgroundColor="red";
         setTimeout(() => {
             document.querySelector("body").style.backgroundColor="white";
@@ -48,23 +48,23 @@ function check(idx){
     }
 }
 
-function btnpressed(){
+function btnpressed(){ 
     let btn=this;
     btn.classList.add("userflash");
     setTimeout(() => {
         btn.classList.remove("userflash");
     }, 250);
-    usercolour=btn.getAttribute("id");
-    userseq.push(usercolour);
-    check(userseq.length-1);
+    usercolour=btn.getAttribute("id"); 
+    userseq.push(usercolour); // adding the colour to user seq.
+    check(userseq.length-1); // checking upto last index.
 }
 
 let allbtn=document.querySelectorAll(".btn");
 for(btn of allbtn){
-    btn.addEventListener("click",btnpressed);
+    btn.addEventListener("click",btnpressed);  // btnpressed function has two works. one is to add the colour to userseq and second is to check for the correct sequence.
 }
 
-function reset(){
+function reset(){  // resets the values to their initial values.
     level=0;
     gameseq=[];
     userseq=[];
